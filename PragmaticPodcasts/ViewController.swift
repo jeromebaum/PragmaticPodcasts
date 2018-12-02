b62fb87dce2b460e12c7212ece57122e28c57e11
Jerome Baum <jerome@jeromebaum.com>
2018-12-02 15:29:26 +0000

    Initial Commit

---
+++ PragmaticPodcasts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PragmaticPodcasts
+//
+//  Created by Jerome Baum on 02/12/2018.
+//  Copyright © 2018 Jerome Baum. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
